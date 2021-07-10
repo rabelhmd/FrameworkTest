8450501a3f82fbe514f5c7a395b1ca06bbd850b8
Rabel Ahmed <rabelhmd@gmail.com>
2021-07-10 12:01:27 +0600

    Validator added.

---
+++ Validator/Validator/Validate.swift
@@ -0,0 +1,15 @@
+//
+//  Validate.swift
+//  Validator
+//
+//  Created by Rabel Ahmed  on 10/7/21.
+//
+
+import Foundation
+
+struct Validate {
+    
+    func sayHello() {
+        print("Hello World")
+    }
+}
